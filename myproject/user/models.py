eb68644484dafa8b67ef6e3fbd3c0a6e35f9b905
baseballclub <abkimdoyo25@gmail.com>
2025-03-28 23:23:08 +0900

    feat : 회원가입 아이디 추가

--- myproject/user/models.py
+++ myproject/user/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 
 class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
-
+    userid = models.CharField(max_length=15, unique=True)
     groups = models.ManyToManyField(Group, related_name="customuser_set", blank=True)
     user_permissions=models.ManyToManyField(Permission, related_name="customeuser_permissions_set",blank=True)
