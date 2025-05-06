c75de94723cd1b33017db542b48062e8467c62f4
baseballclub <abkimdoyo25@gmail.com>
2025-05-07 00:39:03 +0900

    [Feat] 방명록 기능

--- myproject/user/models.py
+++ myproject/user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.conf import settings
 
 # Create your models here.
 
@@ -7,3 +8,9 @@ class CustomUser(AbstractUser):
     nickname = models.CharField(max_length=15, unique=True)
     groups = models.ManyToManyField(Group, related_name="customuser_set", blank=True)
     user_permissions=models.ManyToManyField(Permission, related_name="customeuser_permissions_set",blank=True)
+
+class GuestBook(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='guestbooks')
+    writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='written_guestbooks')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
